bb5e3826cdb99f7fffa0d02cf3e97d3d5ee8a504
gefgu <gefgu@hotmail.com>
2022-07-24 16:24:53 -0300

    Add get posts from reader project

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,4 +1,5 @@
-import { useContext, useEffect } from "react";
+import { DateTime } from "luxon";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 
@@ -12,6 +13,36 @@ function DashboardPage() {
       navigate("/login");
     }
   }, [user]);
+
+  const [posts, setPosts] = useState();
+
+  const getPosts = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`);
+    let data = await response.json();
+
+    data.sort(
+      (a, b) =>
+        new Date(b?.publishedDate || b.creationDate) -
+        new Date(a?.publishedDate || a.creationDate)
+    );
+
+    data = data.map((item) => {
+      let newItem = item;
+      newItem.date = newItem.publishedDate || newItem.creationDate;
+      newItem.date = DateTime.fromISO(newItem.date).toLocaleString(
+        DateTime.DATE_MED
+      );
+      newItem.author = newItem.author.username;
+      return newItem;
+    });
+    return data;
+  };
+
+  useEffect(() => {
+    getPosts().then((data) => setPosts(data));
+
+    return () => setPosts(null);
+  }, []);
 
   return <h1>Hi!</h1>;
 }
