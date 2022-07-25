d9288d288d2e025ddccaa8539154e8a3005ffca6
gefgu <gefgu@hotmail.com>
2022-07-25 14:18:35 -0300

    Add import of post and comment fetching methods from reader project

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,4 +1,54 @@
+import { DateTime } from "luxon";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function PostPage() {
+  const postId = useParams().postId;
+  const [post, setPost] = useState();
+  const [comments, setComments] = useState();
+
+  const getPost = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/posts/${postId}`
+    );
+    let data = await response.json();
+
+    data.author = data.author.username;
+    return data;
+  };
+
+  const getComments = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/posts/${postId}/comments`
+    );
+    let data = await response.json();
+
+    data.sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
+
+    data = data.map((item) => {
+      item.date = item.publishedDate || item.creationDate;
+      item.date = DateTime.fromISO(item.date).toLocaleString(DateTime.DATE_MED);
+      item.author = item.author.username;
+      return item;
+    });
+
+    return data;
+  };
+
+  const updateComments = () => {
+    getComments().then((data) => setComments(data));
+  };
+
+  useEffect(() => {
+    getPost().then((data) => setPost(data));
+    updateComments();
+
+    return () => {
+      setPost(null);
+      setComments(null);
+    };
+  }, []);
+
   return <h1>Hi!</h1>;
 }
 
