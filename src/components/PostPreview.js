fa915af15b9f4302c691bcc9bf7dcafba8ef4052
gefgu <gefgu@hotmail.com>
2022-07-25 16:26:35 -0300

    Add delete button for posts in dashboard"

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -4,24 +4,25 @@ import Heading from "../styled-components/Heading";
 import Paragraph from "../styled-components/Paragraph";
 import SubHeading from "../styled-components/SubHeading";
 import { Link } from "react-router-dom";
+import Button from "../styled-components/Button";
+import Flex from "../styled-components/Flex";
 
 function PostPreview({ post }) {
   return (
     <ContentContainer>
-      <Anchor>
-        <Link to={`/posts/${post._id}`}>
-          <Heading> {post.title}</Heading>
-        </Link>
-      </Anchor>
-      <SubHeading>
-        By {post.author}
-      </SubHeading>
+      <Flex>
+        <Anchor>
+          <Link to={`/posts/${post._id}`}>
+            <Heading> {post.title}</Heading>
+          </Link>
+        </Anchor>
+        <Button>Delete</Button>
+      </Flex>
+      <SubHeading>By {post.author}</SubHeading>
       <SubHeading>
         Published Date: {post.publishedDate || "undefined"}
       </SubHeading>
-      <SubHeading>
-        Creation Date: {post.creationDate}
-      </SubHeading>
+      <SubHeading>Creation Date: {post.creationDate}</SubHeading>
       <Paragraph>{post.content.slice(0, 200)}...</Paragraph>
     </ContentContainer>
   );
